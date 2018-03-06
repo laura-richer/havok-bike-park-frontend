a8e6e846bd908e8de96f1b640d6e96c40bd01d20
laura-richer <lauraricher@bolser.co.uk>
2018-03-06 14:58:07 +0000

    membership page

--- src/app/membership/membership.component.ts
+++ src/app/membership/membership.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiConnections }    from '../services/api-connections.service';
 
 @Component({
   selector: 'app-membership',
@@ -6,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembershipComponent implements OnInit {
 
-  constructor() { }
+  public membership;
+  public pageTitle;
+  public mainCopy;
+  public subParagraph;
+  public benefits;
+  public benefitsIntro;
+
+  constructor(private apiConnections: ApiConnections) {}
 
   ngOnInit() {
+    this.apiConnections.getPage(30)
+      .subscribe(membership => {
+        this.membership = membership;
+        this.pageTitle = membership["title"]["rendered"];
+        this.mainCopy = membership["content"]["rendered"];
+        this.subParagraph = membership["acf"]["sub_paragraph"];
+        this.benefits = membership["acf"]["benefits"];
+        this.benefitsIntro = membership["acf"]["benefits_intro"];
+
+        console.log(this.membership.acf);
+      });
   }
 
 }
